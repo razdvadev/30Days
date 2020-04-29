177c4157c8638ce58b3c0fd4cfad1ce1e6a2a1e5
razdvadev <razdvadev@gmail.com>
2020-04-29 13:36:09 +0300

    learning python

---
+++ Day 9/formatting.py
@@ -0,0 +1,16 @@
+def my_print(txt):
+    print(txt)
+
+msg_template = """
+Hello {name},
+Thank you for joinig {website}. We are very
+happy to have you with us.
+""" # .format(name="Justin", website="cfe.sh")
+
+def format_msg(my_name="Justin", my_website="cfe.sh"):
+    my_msg = msg_template.format(name=my_name, website=my_website)
+    #print(my_msg)
+    return my_msg
+
+def base_function(*args, **kwargs):
+    print(args, kwargs)
